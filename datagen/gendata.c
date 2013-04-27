555ce6c102f1a99ceb9c0b08c7bbee35805bbbf2
Francois Dang Ngoc <francois.dangngoc@gmail.com>
2013-04-27 19:13:38 -0400

    Added csv header

--- datagen/gendata.c
+++ datagen/gendata.c
@@ -11,8 +11,9 @@
 
 int main(int argc, char *argv[]) {
 	int year, month, day, hour, advertiser_id, publisher_id, imps, clicks;
-	double spent;
+	double revenue;
 	srand(time(NULL));
+	printf("ymd,advertiser_id,publisher_id,imps,clicks,revenue\n");
 	for (year = 0; year < NUM_YEARS; year++) {
 	    for (month = 1; month <= NUM_MONTHS; month++) {
 	        for (day = 1; day <= NUM_DAYS; day++) {
@@ -21,11 +22,11 @@ int main(int argc, char *argv[]) {
 	                    for (publisher_id = 0; publisher_id < NUM_PUBLISHERS; publisher_id++) {
 				imps = rand() * 1000000.0 / RAND_MAX;
 				clicks = imps / (rand() * 20.0 / RAND_MAX + 10);
-				spent = rand() * 10.0 * (double)imps / (double)(RAND_MAX * 100000.0);
+				revenue = rand() * 10.0 * (double)imps / (double)(RAND_MAX * 100000.0);
 		        	printf("%d-%02d-%02d %02d:00:00,%d,%d,%d,%d,%f\n",
 					2013 - NUM_YEARS + year,
 					month, day, hour, advertiser_id,
-					publisher_id, imps, clicks, spent);
+					publisher_id, imps, clicks, revenue);
 			    }
 		        }
 		    }
