62b7e5a7b412d05c2bc2a76b667841f3c270ce40
Francois Dang Ngoc <francois.dangngoc@gmail.com>
2013-05-09 22:24:38 -0400

    fixed MySQL query to force to use index

--- datagen/test_mysql.sh
+++ datagen/test_mysql.sh
@@ -10,11 +10,11 @@ USERNAME=$1
 PASSWORD=$2
 DATABASE=$3 
 
-#echo "SET SQL_BIG_SELECTS=1; SELECT SUM(imps) FROM analytics" | /usr/bin/time --portability mysql -u $USERNAME -p$PASSWORD -U $DATABASE
-#echo "SET SQL_BIG_SELECTS=1; SELECT SUM(imps) FROM analytics" | /usr/bin/time --portability mysql -u $USERNAME -p$PASSWORD -U $DATABASE
+echo "SET SQL_BIG_SELECTS=1; SELECT SUM(imps) FROM analytics" | /usr/bin/time --portability mysql -u $USERNAME -p$PASSWORD -U $DATABASE
+echo "SET SQL_BIG_SELECTS=1; SELECT SUM(imps) FROM analytics" | /usr/bin/time --portability mysql -u $USERNAME -p$PASSWORD -U $DATABASE
 
-echo "SET SQL_BIG_SELECTS=1; SELECT advertiser_id, AVG(clicks) FROM analytics WHERE ymdh BETWEEN '2012-06-01 00:00:00' AND '2012-07-01 00:00:00' GROUP BY advertiser_id" | /usr/bin/time --portability mysql -u $USERNAME -p$PASSWORD -U $DATABASE
-echo "SET SQL_BIG_SELECTS=1; SELECT advertiser_id, AVG(clicks) FROM analytics WHERE ymdh BETWEEN '2012-06-01 00:00:00' AND '2012-07-01 00:00:00' GROUP BY advertiser_id" | /usr/bin/time --portability mysql -u $USERNAME -p$PASSWORD -U $DATABASE
+echo "SET SQL_BIG_SELECTS=1; SELECT advertiser_id, AVG(clicks) FROM analytics FORCE INDEX (idx_ymdh) WHERE ymdh BETWEEN '2012-06-01 00:00:00' AND '2012-07-01 00:00:00' GROUP BY advertiser_id" | /usr/bin/time --portability mysql -u $USERNAME -p$PASSWORD -U $DATABASE
+echo "SET SQL_BIG_SELECTS=1; SELECT advertiser_id, AVG(clicks) FROM analytics FORCE INDEX (idx_ymdh) WHERE ymdh BETWEEN '2012-06-01 00:00:00' AND '2012-07-01 00:00:00' GROUP BY advertiser_id" | /usr/bin/time --portability mysql -u $USERNAME -p$PASSWORD -U $DATABASE
 
-#echo "SET SQL_BIG_SELECTS=1; select avg(1000 * revenue / imps) as cpm from analytics where ymdh > '2012-12-01 00:00:00' GROUP BY publisher_id ORDER BY cpm DESC LIMIT 10;" | /usr/bin/time --portability mysql -u $USERNAME -p$PASSWORD -U $DATABASE
-#echo "SET SQL_BIG_SELECTS=1; select avg(1000 * revenue / imps) as cpm from analytics where ymdh > '2012-12-01 00:00:00' GROUP BY publisher_id ORDER BY cpm DESC LIMIT 10;" | /usr/bin/time --portability mysql -u $USERNAME -p$PASSWORD -U $DATABASE
+echo "SET SQL_BIG_SELECTS=1; select avg(1000 * revenue / imps) as cpm from analytics where ymdh > '2012-12-01 00:00:00' GROUP BY publisher_id ORDER BY cpm DESC LIMIT 10;" | /usr/bin/time --portability mysql -u $USERNAME -p$PASSWORD -U $DATABASE
+echo "SET SQL_BIG_SELECTS=1; select avg(1000 * revenue / imps) as cpm from analytics where ymdh > '2012-12-01 00:00:00' GROUP BY publisher_id ORDER BY cpm DESC LIMIT 10;" | /usr/bin/time --portability mysql -u $USERNAME -p$PASSWORD -U $DATABASE
